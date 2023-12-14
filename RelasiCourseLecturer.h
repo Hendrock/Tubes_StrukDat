6b0acd2773ab4f342a28cd3c13faf5fdffb63d33
Hendrock <107454519+Hendrock@users.noreply.github.com>
2023-12-14 11:35:10 +0700

    Update RelasiCourseLecturer.h

--- RelasiCourseLecturer.h
+++ RelasiCourseLecturer.h
@@ -1,6 +1,38 @@
 #ifndef RELASICOURSELECTURER_H_INCLUDED
 #define RELASICOURSELECTURER_H_INCLUDED
 
+#include "Lecturer.h"
+#include "Course.h"
 
+#define next(P) P->next
+#define child(P) P->child
+#define parent(P) P->parent
+#define first(L) L.first
+#define last(L) L.last
+
+typedef struct elmlistRelasiCourseLecturer *addressRelasiCourseLecturer;
+
+struct elmlistRelasiCourseLecturer{
+    addressRelasiCourseLecturer next;
+    addressCourse parent;
+    addressLecturer child;
+};
+
+struct ListRelasiCourseLecturer{
+    addressRelasiCourseLecturer first, last;
+};
+
+void createListRelasiCourseLecturer(ListRelasiCourseLecturer &L);
+void insertFirstRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer P);
+void insertLastRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer P);
+void insertAfterRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer Prec, addressRelasiCourseLecturer P);
+void deleteFirstRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer &P);
+void deleteLastRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer &P);
+void deleteAfterRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer Prec, addressRelasiCourseLecturer &P);
+
+addressRelasiCourseLecturer alokasi(addressCourse P, addressLecturer C);
+void dealokasi(addressRelasiCourseLecturer &P);
+addressRelasiCourseLecturer findElm(ListRelasiCourseLecturer L, addressCourse P, addressLecturer C);
+void printInfo(ListRelasiCourseLecturer L);
 
 #endif // RELASICOURSELECTURER_H_INCLUDED
