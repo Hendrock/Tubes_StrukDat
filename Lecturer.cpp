5245f741a839c7f0747805150c404ccff9bd3466
Hendrock <107454519+Hendrock@users.noreply.github.com>
2023-12-14 19:42:32 +0700

    Update Lecturer.cpp

--- Lecturer.cpp
+++ Lecturer.cpp
@@ -1,72 +1,128 @@
 #include "Lecturer.h"
 
-
-void createList(List_child &L) {
+void createListLecturer(ListLecturer &L) {
     first(L) = NULL;
     last(L) = NULL;
 }
 
-void insertFirst(ListLecturer &L, addressLecturer P){
-    if (first(L) == NULL){
-        first(L) = P;
-        last(L) = P;
-        next(P) = P;
-    } else {
-        next(P) = first(L);
-        next(last(L)) = P;
-        first(L) = P;
-    }
-}
-
-void insertLast(ListLecturer &L, addressLecturer P){
-    next(P) = first(L);
-    next(last(L)) = P;
-    last(L) = P;
-}
-
-void insertAfter(addressLecturer Prec, addressLecturer P){
-
-}
-
-void deleteFirst(ListLecturer &L, addressLecturer &P);
-void deleteLast(ListLecturer &L, addressLecturer &P);
-void deleteAfter(addressLecturer Prec, addressLecturer &P);
-
-addressLecturer alokasi(infotypeLecturer x){
+addressLecturer alokasiLecturer(infotypeLecturer x) {
     addressLecturer P = new elmlistLecturer;
     info(P) = x;
     next(P) = NULL;
     return P;
 }
 
-void dealokasi(addressLecturer &P){
-    delete (P);
+void dealokasiLecturer(addressLecturer &P) {
+    delete P;
 }
 
-addressLecturer findElm(ListLecturer L, string x){
-    addressLecturer P = first(L);
+void insertFirstLecturer(ListLecturer &L, addressLecturer P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+        last(L) = P;
+        next(P) = P;
+    } else {
+        next(P) = first(L);
+        last(L)->next = P;
+        first(L) = P;
+    }
+}
 
-    do{
-        if (info(P).kode == x){
+void insertLastLecturer(ListLecturer &L, addressLecturer P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+        last(L) = P;
+        next(P) = P;
+    } else {
+        next(last(L)) = P;
+        next(P) = first(L);
+        last(L) = P;
+    }
+}
+
+void insertAfterLecturer(ListLecturer &L, addressLecturer Prec, addressLecturer P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+        last(L) = P;
+        next(P) = P;
+    } else if (Prec == last(L)) {
+        next(Prec) = P;
+        next(P) = first(L);
+        last(L) = P;
+    } else {
+        next(P) = next(Prec);
+        next(Prec) = P;
+    }
+}
+
+void deleteFirstLecturer(ListLecturer &L, addressLecturer &P) {
+    if (first(L) == NULL) {
+        cout << "List Kosong" << endl;
+    } else {
+        P = first(L);
+        if (first(L) == last(L)) {
+            first(L) = NULL;
+            last(L) = NULL;
+        } else {
+            first(L) = next(P);
+            next(last(L)) = first(L);
+        }
+        next(P) = NULL;
+    }
+}
+
+void deleteLastLecturer(ListLecturer &L, addressLecturer &P) {
+    if (first(L) == NULL) {
+        cout << "List Kosong" << endl;
+    } else {
+        P = first(L);
+        if (first(L) == last(L)) {
+            first(L) = NULL;
+            last(L) = NULL;
+        } else {
+            while (next(P) != last(L)) {
+                P = next(P);
+            }
+            next(P) = first(L);
+            last(L) = P;
+            P = next(P);
+        }
+    }
+}
+
+void deleteAfterLecturer(ListLecturer &L, addressLecturer Prec, addressLecturer &P) {
+    if (first(L) == NULL) {
+        cout << "List Kosong" << endl;
+    } else {
+        P = next(Prec);
+        next(Prec) = next(P);
+        next(P) = NULL;
+    }
+}
+
+addressLecturer findElmLecturer(ListLecturer L, string kode) {
+    addressLecturer P = first(L);
+    do {
+        if (info(P).kode == kode) {
             return P;
         }
         P = next(P);
-    }(while P != NULL);
+    } while (P != first(L));
+    return NULL;
 }
 
-void printInfo(ListLecturer L){
-    addressLecturer P = first(L);
-
+void printInfoLecturer(ListLecturer L) {
     cout << "-----------------------------------" << endl;
-    if (P == NULL){
-        cout << "List Kosong" << endl;
-    } else {
-        do{
-            cout << "Nama Dosen: " <<  info(P).nama << endl;
-            cout << "Kode Dosen: " <<  info(P).kode << endl;
+    if (first(L) != NULL) {
+        addressLecturer P = first(L);
+        do {
+            cout << "Nama: " << info(P).nama << endl;
+            cout << "Kode: " << info(P).kode << endl;
             cout << endl;
             P = next(P);
-        }(while P != NULL);
+        } while (P != first(L));
+    } else {
+        cout << "List Kosong" << endl;  
     }
     cout << "-----------------------------------" << endl;
 }
