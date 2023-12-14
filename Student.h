918d02324d28c2d05136b092362b566e4d116873
Hendrock <107454519+Hendrock@users.noreply.github.com>
2023-12-14 19:33:47 +0700

    Update Student.h

--- Student.h
+++ Student.h
@@ -7,16 +7,15 @@
 using namespace std;
 
 #define next(P) P->next
+#define info(P) P->info
 #define first(L) L.first
 #define last(L) L.last
-#define info(P) P->info
 
 typedef struct Student infotypeStudent;
 typedef struct elmlistStudent *addressStudent;
 
 struct Student{
     string nama, nim, email, password;
-    int nilai;
 };
 
 struct elmlistStudent{
@@ -28,17 +27,17 @@ struct ListStudent{
     addressStudent first, last;
 };
 
-void createList(ListStudent &L);
-void insertFirst(ListStudent &L, addressStudent P);
-void insertLast(ListStudent &L, addressStudent P);
-void insertAfter(addressStudent Prec, addressStudent P);
-void deleteFirst(ListStudent &L, addressStudent &P);
-void deleteLast(ListStudent &L, addressStudent &P);
-void deleteAfter(addressStudent Prec, addressStudent &P);
+void createListStudent(ListStudent &L);
+void insertFirstStudent(ListStudent &L, addressStudent P);
+void insertLastStudent(ListStudent &L, addressStudent P);
+void insertAfterStudent(ListStudent &L, addressStudent Prec, addressStudent P);
+void deleteFirstStudent(ListStudent &L, addressStudent &P);
+void deleteLastStudent(ListStudent &L, addressStudent &P);
+void deleteAfterStudent(ListStudent &L, addressStudent Prec, addressStudent &P);
 
-addressStudent alokasi(infotypeStudent x);
-void dealokasi(addressStudent &P);
-addressStudent findElm(ListStudent L, infotypeStudent x);
-void printInfo(ListStudent L);
+addressStudent alokasiStudent(infotypeStudent x);
+void dealokasiStudent(addressStudent &P);
+addressStudent findElmStudent(ListStudent L, string nim);
+void printInfoStudent(ListStudent L);
 
 #endif // STUDENT_H_INCLUDED
