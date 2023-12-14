7908ce9873b22bf0dd1fec815cf0af1c11e8e7d8
Hendrock <107454519+Hendrock@users.noreply.github.com>
2023-12-14 13:31:09 +0700

    Update Command.cpp

--- Command.cpp
+++ Command.cpp
@@ -2,6 +2,54 @@
 #include "Lecturer.h"
 #include "Course.h"
 
+void sortStudentScore(ListCourse LC, bool permission){
+    if (!permission){
+        cout << "Anda tidak dapat menggunakan fitur ini" << endl; 
+    } else {
+        string tempKode;
+
+        cout << "Masukkan kode kelas yang ingin di sort: ";
+        cin >> tempKode;
+
+        addressCourse P = findElmCourse(LC, tempKode);
+
+        if (P == NULL){
+            cout << "Kelas tidak ditemukan." << endl;
+        } else {
+            string choice;
+
+            cout << "Ascending/Descending? (A/D)";
+            cin >> choice;
+            
+            for (int i = 0; i < NMAX-1; i++){
+		        int index = i;
+                for (int j = i+1; j < NMAX; j++){
+                    if (choice == "A"){
+                        if (info(P).menjawab[j].score < info(P).menjawab[idx].score
+                    }
+                }
+		switch opt {
+		case 0: // Ascending
+			for j := i + 1; j < NMAX; j++ {
+				if (*T)[j].Kategori == category && (*T)[j].Stok < (*T)[idx].Stok {
+					idx = j
+				}
+			}
+		case 1: // Descending
+			for j := i + 1; j < NMAX; j++ {
+				if (*T)[j].Kategori == category && (*T)[j].Stok > (*T)[idx].Stok {
+					idx = j
+				}
+			}
+		}
+
+		(*T)[i], (*T)[idx] = (*T)[idx], (*T)[i]
+	}
+            }
+        }
+    }
+}
+
 void jawabQuiz(ListCourse LC, ListStudent &LS, bool permission){
     if (!permission){
         cout << "Anda tidak dapat menggunakan fitur ini" << endl; 
