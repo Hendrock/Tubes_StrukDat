ff7d668ced7d5bb5b06bfdac71b25a0388a37e60
Hendrock <107454519+Hendrock@users.noreply.github.com>
2023-12-14 19:41:49 +0700

    Update RelasiCourseLecturer.cpp

--- RelasiCourseLecturer.cpp
+++ RelasiCourseLecturer.cpp
@@ -1,93 +1,92 @@
 #include "RelasiCourseLecturer.h"
-#include "Course.h"
-#include "Lecturer.h"
 
-void createListRelasiCourseLecturer(ListRelasiCourseLecturer &L){
+void createListRelasiCourseLecturer(ListRelasiCourseLecturer &L) {
     first(L) = NULL;
     last(L) = NULL;
 }
 
-void insertFirstRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer P){
-    if (first(L) == NULL){
+addressRelasiCourseLecturer alokasiRelasiCourseLecturer(addressCourse course, addressLecturer Lecturer) {
+    addressRelasiCourseLecturer P = new elmlistRelasiCourseLecturer;
+    parent(P) = course;
+    child(P) = Lecturer;
+    next(P) = NULL;
+    return P;
+}
+
+void dealokasiRelasiCourseLecturer(addressRelasiCourseLecturer &P) {
+    delete P;
+}
+
+void insertFirstRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer P) {
+    if (first(L) == NULL) {
         first(L) = P;
+        last(L) = P;
     } else {
         next(P) = first(L);
         first(L) = P;
     }
 }
 
-void insertLastRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer P){
-    if (fisrt(L) == NULL){
-        first(L) == P;
+void insertLastRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+        last(L) = P;
     } else {
         next(last(L)) = P;
         last(L) = P;
     }
 }
 
-void insertAfterRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer Prec, addressRelasiCourseLecturer P){
-    if (first(L) == NULL){
+void insertAfterRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer Prec, addressRelasiCourseLecturer P) {
+    if (first(L) == NULL) {
         first(L) = P;
+        last(L) = P;
     } else {
         next(P) = next(Prec);
         next(Prec) = P;
-    }        
+    }
 }
 
-void deleteFirstRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer &P){
-    if (first(L) == NULL){
+void deleteFirstRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer &P) {
+    if (first(L) == NULL) {
         cout << "List Kosong" << endl;
-    } else if (next(first(L)) == NULL){
-        P = first(L);
-        first(L) = NULL;
-        last(L) = NULL;
     } else {
         P = first(L);
-        first(L) = next(P);
+        if (first(L) == last(L)) {
+            first(L) = NULL;
+            last(L) = NULL;
+        } else {
+            first(L) = next(P);
+        }
         next(P) = NULL;
     }
 }
 
-void deleteLastRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer &P){
-    if (first(L) == NULL){
+void deleteLastRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer &P) {
+    if (first(L) == NULL) {
         cout << "List Kosong" << endl;
-    } else if (next(first(L)) == NULL){
-        P = first(L);
-        first(L) = NULL;
-        last(L) = NULL;
     } else {
-        addressLastRelasiCourseLecturer Q;
         P = first(L);
-        while (P != NULL){
-            Q = P;
+        if (first(L) == last(L)) {
+            first(L) = NULL;
+            last(L) = NULL;
+        } else {
+            while (next(P) != last(L)) {
+                P = next(P);
+            }
+            next(P) = NULL;
+            last(L) = P;
             P = next(P);
         }
-        next(Q) = NULL;
-        last(L) = Q; 
     }
 }
 
-void deleteAfterRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer Prec, addressRelasiCourseLecturer &P){
-    if (first(L) == NULL){
+void deleteAfterRelasiCourseLecturer(ListRelasiCourseLecturer &L, addressRelasiCourseLecturer Prec, addressRelasiCourseLecturer &P) {
+    if (first(L) == NULL) {
         cout << "List Kosong" << endl;
-    } else if (next(first(L)) == NULL){
-        P = first(L);
-        first(L) = NULL;
     } else {
         P = next(Prec);
         next(Prec) = next(P);
         next(P) = NULL;
     }
-}
-
-addressRelasiCourseLecturer alokasiRelasiCourseLecturer(addressCourse P, addressLecturer Q){
-    addressRelasiCourseLecturer P = new elmlistRelasiCourseLecturer;
-    parent(L) = P;
-    child(L) = Q;
-    next(P) = NULL;
-    return P;
-}
-
-void dealokasiRelasiCourseLecturer(addressRelasiCourseLecturer &P){
-    delete (P);
 }
